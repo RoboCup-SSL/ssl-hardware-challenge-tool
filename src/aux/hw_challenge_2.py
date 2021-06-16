a7d70cdb0b5fffef448bd8607bae54b6811bb42d
Leonardo Costa <leosilvacosta6@gmail.com>
2021-06-16 19:22:08 -0300

    Add 0.5s between HALT and FORCE_START in challenge 2

--- src/aux/hw_challenge_2.py
+++ src/aux/hw_challenge_2.py
@@ -4,7 +4,7 @@ from aux.RobotBall import Position
 
 
 class Challenge_2(object):
-    max_steps = 2
+    max_steps = 4
     id = 2
     max_attack_robots = 0
     min_attack_robots = 0
@@ -18,11 +18,23 @@ class Challenge_2(object):
             return Challenge_2.Step_1()
         elif step == ChallengeSteps.STEP_2:
             return Challenge_2.Step_2()
+        elif step == ChallengeSteps.STEP_3:
+            return Challenge_2.Step_3()
+        elif step == ChallengeSteps.STEP_4:
+            return Challenge_2.Step_4()
 
     @staticmethod
     def Step_1() -> Action:
-        return Action(True, command=GCCommands.FORCE_START)
+        return Action(False, command=GCCommands.STOP)
 
     @staticmethod
     def Step_2() -> Action:
+        return Action(False, timer=0.5)
+
+    @staticmethod
+    def Step_3() -> Action:
+        return Action(True, command=GCCommands.FORCE_START)
+
+    @staticmethod
+    def Step_4() -> Action:
         return Action(False, timer=30)
